06fc5bb2da54ad92dccf28f572d026606db16d64
Colin Morris <relapse@gmail.com>
2022-07-30 16:45:29 +1000

    Circle buttons too padded

--- src/character.go
+++ src/character.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -132,4 +136,57 @@ func characterWindow(app *fyne.App, charName string) fyne.Window {
 	mainWindow.SetContent(widget.NewLabel("Hello"))
 	return mainWindow
 
+}
+
+type DotButton struct {
+	Max     int
+	Min     int
+	Value   int
+	Buttons []*widget.Button
+}
+
+func (d *DotButton) SetValue(value int) {
+	d.Value = value
+	// Set button appearances
+}
+
+func (d *DotButton) ToCanvas() *fyne.Container {
+	x := fyne.NewContainerWithLayout(
+		layout.NewHBoxLayout(),
+	)
+	for _, y := range d.Buttons {
+		x.Objects = append(x.Objects, y)
+	}
+	return x
+}
+
+func MakeDotButtons(max int, min int) DotButton {
+	var d DotButton
+	d.Max = max
+	d.Min = min
+	d.Buttons = make([]*widget.Button, max)
+	for x := range d.Buttons {
+		z := x
+		d.Buttons[x] = widget.NewButtonWithIcon("", theme.RadioButtonIcon(), func() {
+			thisValue := z + 1
+			// @todo: Check if this is a valid value
+			if thisValue < d.Min || thisValue == d.Value {
+				fmt.Printf("Can't go below min")
+				d.Value = d.Min
+			} else {
+				d.Value = thisValue
+			}
+			for mep := 0; mep < d.Value; mep++ {
+				d.Buttons[mep].Icon = theme.RadioButtonCheckedIcon()
+				d.Buttons[mep].Refresh()
+			}
+			for mep := d.Value; mep < d.Max; mep++ {
+				d.Buttons[mep].Icon = theme.RadioButtonIcon()
+				d.Buttons[mep].Refresh()
+			}
+			fmt.Printf("Tapped %d\n", z+1)
+		})
+		d.Buttons[x].Importance = widget.LowImportance
+	}
+	return d
 }
--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
 	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -13,13 +17,14 @@ func main() {
 	app := app.New()
 	mainWindow := setupMainWindow(&app)
 
+	funSetup(&mainWindow)
 	mainWindow.Show()
 	app.Run()
 }
 
 func setupMainWindow(app *fyne.App) fyne.Window {
 	mainWindow := (*app).NewWindow("Trinity Continuum")
-	mainWindow.Resize(fyne.NewSize(640, 480))
+	mainWindow.Resize(fyne.NewSize(640, 960))
 	mainMenu := fyne.NewMainMenu(
 		fyne.NewMenu("Trinity"),
 		fyne.NewMenu("Characters",
@@ -35,8 +40,38 @@ func setupMainWindow(app *fyne.App) fyne.Window {
 			}),
 		))
 	mainWindow.SetMainMenu(mainMenu)
+	mainWindow.SetMaster()
 
 	content := container.NewBorder(nil, nil, nil, nil, widget.NewLabel("Content"))
 	mainWindow.SetContent(content)
 	return mainWindow
+}
+
+func funSetup(window *fyne.Window) {
+	mep0 := MakeDotButtons(5, 1)
+	mep1 := MakeDotButtons(5, 1)
+	mep2 := MakeDotButtons(5, 1)
+	mep3 := widget.NewSelect(
+		[]string{
+			"●oooo",
+			"●●ooo",
+			"●●●oo",
+			"●●●●o",
+			"●●●●●",
+		},
+		func(s string) { fmt.Printf("Changed\n") },
+	)
+	statLine := container.New(
+		layout.NewFormLayout(),
+		canvas.NewText("Might", color.Black),
+		mep0.ToCanvas(),
+		canvas.NewText("Dexterity", color.Black),
+		mep1.ToCanvas(),
+		canvas.NewText("Resilience", color.Black),
+		mep2.ToCanvas(),
+		canvas.NewText("Intelligence", color.Black),
+		mep3,
+	)
+
+	(*window).SetContent(statLine)
 }
